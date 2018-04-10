967e259ea97e7a426d9c4c084c8b6a5847fbc7af
jthai2 <jimmythai35@gmail.com>
2018-04-10 12:53:09 -0500

    l

---
+++ Guessing Game/Guessing Game/Source.cpp
@@ -0,0 +1,34 @@
+//Guessing Game
+//04/10/18
+//Jimmy Thai
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>    /* srand, rand*/
+#include <time.h>      /* time */
+#include <random>
+
+int main()
+{
+	int numChoice;
+	int secret;
+
+	srand(time(NULL));
+	/* generate secret number between 1 and 10: */
+	secret = rand() % 10 + 1;
+
+	std::cout << "Please pick a number between 1-10" << std::endl;
+	std::cin >> numChoice;
+
+	if (numChoice == secret)
+	{
+		std::cout << "Congratulations, you guessed " << secret << " the correct number." << std::endl;
+	}
+	else
+	{
+		std::cout << "You chose poorly... the number was " << secret << std::endl;
+	}
+
+	system("pause");
+	return 0;
+}
